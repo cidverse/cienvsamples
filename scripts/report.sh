99d53afc7cc218e975bd7e6d10f622142ac922ef
Philipp Heuer <git@philippheuer.me>
2022-05-08 03:33:22 +0200

    feat: add report.sh

---
+++ scripts/report.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# log
+echo "collect for: ${CI_SERVICE_NAME}"
+
+# collect
+printenv > ${CI_SERVICE_NAME}.env
+
+# log result
+echo "environment:"
+cat ${CI_SERVICE_NAME}.env
+
+# install gh cli
+GH_VERSION=2.9.0
+curl -sSL https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.tar.gz -o /tmp/gh_${GH_VERSION}_linux_amd64.tar.gz
+tar xvf /tmp/gh_${GH_VERSION}_linux_amd64.tar.gz -C /tmp
+cp /tmp/gh_${GH_VERSION}_linux_amd64/bin/gh /usr/local/bin/
+chmod +x /usr/local/bin/gh
+
+# store result
+gh auth login --with-token <<<"$GH_GIST_TOKEN"
+gh gist create --public ${CI_SERVICE_NAME}.env -d "${CI_SERVICE_NAME}"
